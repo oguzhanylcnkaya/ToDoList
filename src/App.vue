c3c93f41aac016d136c152f23c2d318d507eb8ed
Oğuzhan Yalçınkaya <Oguzhanyalcinkayaa@gmail.com>
2021-01-20 20:57:13 +0300

    add project to github

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input type="text" v-model="job"/>
+    <input type="button" @click="addJob" value="Add" />
+
+    <h1>TO DO LİST</h1>
+    <hr />
+    <ul v-for="(item, index) in todolist" :key="item">
+      <li>{{item}}</li><button @click="deleteJob(index)">X</button>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      job : "",
+      todolist : []
+    }
+  },
+  methods : {
+    addJob(){
+      this.todolist.push(this.job);
+      this.job = "";
+    },
+    deleteJob(index){
+      this.todolist.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
